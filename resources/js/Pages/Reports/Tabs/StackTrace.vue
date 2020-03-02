8171b0e45e1b7ddf0e56afecab3f383f9c05cb34
Pieter Jan Pollie <pieterjanpollie@gmail.com>
2020-03-02 16:20:14 +0100

    Made stack trace line numbers unselectable + added syntax highlightin

--- resources/js/Pages/Reports/Tabs/StackTrace.vue
+++ resources/js/Pages/Reports/Tabs/StackTrace.vue
@@ -21,7 +21,8 @@
                          :class="frame.is_application_frame ? 'text-indigo-700' : 'text-gray-600'"
                          v-html="formatFileName(frame.file)">
                     </div>
-                    <span class="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium leading-4 bg-gray-100 text-gray-600 absolute right-0 bottom-0">
+                    <span
+                        class="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium leading-4 bg-gray-100 text-gray-600 absolute right-0 bottom-0">
                           :{{ frame.line_number }}
                     </span>
                 </div>
@@ -36,7 +37,7 @@
 
                 <div class="bg-gray-200">
                     <p v-for="(code, nr) in activeFrame.code_snippet" :key="nr"
-                       style="line-height: 2" class="text-gray-500 text-xs px-2"
+                       style="line-height: 2" class="text-gray-500 text-xs px-2 select-none"
                        :class="parseInt(activeFrame.line_number) === parseInt(nr) ? 'bg-indigo-200' : ''">
                         {{ nr }}
                     </p>
@@ -47,7 +48,7 @@
                         <pre v-for="(code, nr) in activeFrame.code_snippet" :key="nr"
                              class="text-xs px-2 hover:bg-indigo-100 max-w-full"
                              :class="parseInt(activeFrame.line_number) === parseInt(nr) ? 'bg-indigo-100' : ''"
-                             style="line-height: 2;height: 24px;">{{ code }}</pre>
+                             style="line-height: 2;height: 24px;" v-html="highlightedCode(code)"></pre>
                     </div>
                 </div>
             </div>
@@ -56,12 +57,18 @@
 </template>
 
 <script>
+    import hljs from 'highlight.js/lib/highlight';
+    hljs.registerLanguage('php', require('highlight.js/lib/languages/php'));
+
     export default {
         data() {
             return {
                 activeFrameId: this.$page.report.vars.frames.length - 1,
-                expandVendorFrames: false
+                expandVendorFrames: false,
             };
+        },
+        created() {
+            this.highlightState = null;
         },
         computed: {
             activeFrame() {
@@ -90,7 +97,13 @@
                     return "border-gray-400";
                 else
                     return "border-gray-200";
-            }
+            },
+
+            highlightedCode(code) {
+                const result = hljs.highlight('php', code || '', true, this.highlightState);
+                this.highlightState = result.top;
+                return result.value || '&nbsp;';
+            },
         }
     }
 </script>
